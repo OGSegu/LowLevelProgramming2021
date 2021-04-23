3d1ed06546ed03c94d6644aafe5c65426ba36263
OGSegu <seguisreal@gmail.com>
2021-04-23 15:56:11 +0300

    Added method deleteEdge

--- Lab2/graph.c
+++ Lab2/graph.c
@@ -37,9 +37,26 @@ struct Node *createNode(int number) {
     return node;
 }
 
+int isEdgeExists(struct Graph *graph, struct Node *firstNode, struct Node *secondNode) {
+    int firstNodeValue = firstNode->number;
+    int secondNodeValue = secondNode->number;
+    for (int i = 0; i < graph->edgeList->amount; i++) {
+        int localFirstNodeValue = graph->edgeList[i].value->firstNode->number;
+        int localSecondNodeVValue = graph->edgeList[i].value->secondNode->number;
+        if ((localFirstNodeValue == firstNodeValue || localFirstNodeValue == secondNodeValue)
+            && (localSecondNodeVValue == firstNodeValue || localSecondNodeVValue == secondNodeValue))
+            return 1;
+    }
+    return 0;
+}
+
 void addEdge(struct Graph *graph, int firstNodeNumber, int secondNodeNumber, int weight) {
     struct Node *firstNode = createNode(firstNodeNumber);
     struct Node *secondNode = createNode(secondNodeNumber);
+    if (isEdgeExists(graph, firstNode, secondNode) == 1) {
+        printf("Edge already exists\n");
+        return;
+    }
     struct Edge *edge = malloc(sizeof(struct Edge));
     edge->firstNode = firstNode;
     edge->secondNode = secondNode;
@@ -47,6 +64,26 @@ void addEdge(struct Graph *graph, int firstNodeNumber, int secondNodeNumber, int
     int amount = graph->edgeList->amount;
     graph->edgeList[amount].value = edge;
     graph->edgeList->amount++;
+}
+
+
+void alignListAfterRemove(struct Graph *graph, int emptyValue) {
+    for (int i = emptyValue; i <= graph->edgeList->amount; i++) {
+        graph->edgeList[i].value = graph->edgeList[i + 1].value;
+    }
+}
+
+void removeEdge(struct Graph *graph, int firstNodeValue, int secondNodeValue) {
+    for (int i = 0; i < graph->edgeList->amount; i++) {
+        int localFirstNodeValue = graph->edgeList[i].value->firstNode->number;
+        int localSecondNodeVValue = graph->edgeList[i].value->secondNode->number;
+        if ((localFirstNodeValue == firstNodeValue || localFirstNodeValue == secondNodeValue)
+            && (localSecondNodeVValue == firstNodeValue || localSecondNodeVValue == secondNodeValue)) {
+            alignListAfterRemove(graph, i);
+            graph->edgeList->amount--;
+            return;
+        }
+    }
 }
 
 void printAllEdges(struct Graph *graph) {
@@ -61,8 +98,9 @@ int main() {
     int amountOfNode = 2;
     struct Graph *graph = createGraph(amountOfNode);
     addEdge(graph, 1, 2, 2);
-    addEdge(graph, 6, 1, 2);
-    addEdge(graph, 2, 4, 2);
-    addEdge(graph, 3, 5, 2);
+    addEdge(graph, 3, 4, 2);
+    addEdge(graph, 2, 3, 2);
+    addEdge(graph, 1, 5, 2);
+    removeEdge(graph, 1, 2);
     printAllEdges(graph);
 }
